a39f46fd865a0b6b70d5688f9e7950bb8d99cc73
Igor Souza <igor.ferreira.souza@gmail.com>
2021-10-04 23:36:50 -0300

    start airbnb

--- airbnb/components/HeaderComponent.js
+++ airbnb/components/HeaderComponent.js
@@ -1,10 +1,18 @@
+import Image from "next/image";
 function HeaderComponent() {
     return (
-        <header>
-            <div>
+        <header className="sticky top-0 z-50 grid grid-cols-3 p-5 bg-white shadow-lg md:px-10">
+            <div className="relative flex items-center h-10 my-auto cursor-pointer">
                 <Image
+                    src="https://links.papareact.com/qd3"
+                    layout='fill'
+                    objectFit='contain'
+                    objectPosition="left"
+                />
             </div>
-            <div></div>
+            <div>
+                <input type="text" />
+            </div>
             <div></div>
         </header>
     )

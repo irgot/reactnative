eb3433b63fe9c7f97afdb101d4757715b27c20dc
Igor Souza <igor.ferreira.souza@gmail.com>
2021-10-05 16:35:59 -0300

    inicial airbnb

--- airbnb/components/HeaderComponent.js
+++ airbnb/components/HeaderComponent.js
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import { SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon, UserIcon } from "@heroicons/react/solid";
 function HeaderComponent() {
     return (
-        <header className="sticky top-0 z-50 grid grid-cols-3 p-5 bg-white shadow-lg md:px-10">
+        <header className="sticky top-0 z-50 grid grid-cols-3 p-5 bg-white shadow-sm md:px-10">
             <div className="relative flex items-center h-10 my-auto cursor-pointer">
                 <Image
                     src="https://links.papareact.com/qd3"
@@ -10,10 +11,20 @@ function HeaderComponent() {
                     objectPosition="left"
                 />
             </div>
-            <div>
-                <input type="text" />
+            <div className="flex items-center  rounded-full py-2 md:border-2 md:shadow-lg">
+                <input className=" flex-grow pl-5 bg-transparent outline-none mr-2 text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search" />
+                <SearchIcon
+                    className="h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer hidden md:inline-flex mx-auto md:mx-2"
+                />
             </div>
-            <div></div>
+            <div className="flex items-center space-x-4 justify-end text-gray-500">
+                <p className="hidden md:inline cursor-pointer">Become a host</p>
+                <GlobeAltIcon className="h-6" />
+                <div className="flex items-center space-x-2 border-2 p-2 rounded-full cursor-pointer">
+                    <MenuIcon className='h-6' />
+                    <UserCircleIcon className='h-6' />
+                </div>
+            </div>
         </header>
     )
 }
--- airbnb/pages/index.js
+++ airbnb/pages/index.js
@@ -1,15 +1,66 @@
 import Head from 'next/head'
+import BannerComponent from '../components/BannerComponent'
+import FooterComponent from '../components/FooterComponent'
 import HeaderComponent from '../components/HeaderComponent'
+import LargeCard from '../components/LargeCard'
+import MediumCard from '../components/MediumCard'
+import SmallCard from '../components/SmallCard'
 
-export default function Home() {
-  return (
-    <div className="">
-      <Head>
-        <title>Airbnb</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <HeaderComponent></HeaderComponent>
-      {/* Banner */}
-    </div>
-  )
+export default function Home({ exploreData, cardsData }) {
+    return (
+        <div className="">
+            <Head>
+                <title>Airbnb</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <HeaderComponent />
+            <BannerComponent />
+            <main className="max-w-7xl mx-auto px-8 sm:px-16">
+                <section className="pt-6">
+                    <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                        {exploreData?.map(({ img, distance, location }, index) => {
+                            return (
+                                <SmallCard key={index} img={img} distance={distance} location={location} />
+                            )
+                        })}
+                    </div>
+                </section>
+                <section>
+                    <h2 className="text-4xl font-semibold py-10">Live Anywhere</h2>
+                    <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3 ">
+                        {cardsData?.map(({ img, title }, index) => {
+                            return (
+                                <MediumCard key={index} img={img} title={title} />
+                            )
+                        })}
+                    </div>
+                </section>
+                <LargeCard
+                    img="https://links.papareact.com/4cj"
+                    title="The Greatest Outdoors"
+                    description="Wishlists curated by Airbnb"
+                    buttonText="Get inspired"
+                />
+            </main>
+            <FooterComponent />
+        </div>
+    )
+}
+
+
+export async function getStaticProps() {
+    const exploreData = await fetch('https://links.papareact.com/pyp').then(res => {
+        return (res.json())
+    })
+    const cardsData = await fetch('https://links.papareact.com/zp1').then(res => {
+        return (res.json())
+    })
+
+    return {
+        props: {
+            exploreData,
+            cardsData
+        }
+    }
 }
